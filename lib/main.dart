9dd6ea981aec7db10399207e7b9b3b57049d76e6
Shashank Sundar <shashanksundar16@gmail.com>
2024-04-06 10:53:37 +0530

    login functionality added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:medimind_app/firebase_options.dart';
+import 'package:medimind_app/screens/auth/login_screen.dart';
+import 'package:medimind_app/screens/home_screen.dart';
+import 'package:medimind_app/services/firebase_auth_methods.dart';
+import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -9,61 +20,36 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MediMind',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MultiProvider(
+      providers: [
+        Provider<FirebaseAuthMethods>(
+          create: (_) => FirebaseAuthMethods(FirebaseAuth.instance),
+        ),
+        StreamProvider(
+          create: (context) => context.read<FirebaseAuthMethods>().authState,
+          initialData: null,
+        ),
+      ],
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: AuthWrapper(),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class AuthWrapper extends StatelessWidget {
+  const AuthWrapper({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    final firebaseUser = context.watch<User?>();
+
+    if (firebaseUser != null) {
+      return const HomeScreen();
+      //return const ItineraryEntryPage();
+    } else {
+      return const LoginScreen();
+    }
   }
 }
---
+++ lib/services/firebase_auth_methods.dart
@@ -0,0 +1,171 @@
+// ignore_for_file: use_build_context_synchronously
+
+//import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:medimind_app/screens/auth/login_screen.dart';
+import 'package:medimind_app/screens/home_screen.dart';
+import 'package:medimind_app/utils/snack_bar.dart';
+import 'package:medimind_app/widgets/otp_dialog_box.dart';
+
+class FirebaseAuthMethods {
+  final FirebaseAuth _auth;
+  FirebaseAuthMethods(this._auth);
+
+  //State Management
+  Stream<User?> get authState => FirebaseAuth.instance.authStateChanges();
+
+  User get user => _auth.currentUser!;
+
+  //SignUp using Email
+  Future<void> signUpWithEmail({
+    required String email,
+    required String password,
+    required BuildContext context,
+  }) async {
+    try {
+      await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      Navigator.of(context).pushAndRemoveUntil(
+        MaterialPageRoute(
+          builder: (context) {
+            return const HomeScreen();
+          },
+        ),
+        (route) => false,
+      );
+    } on FirebaseAuthException catch (e) {
+      displaySnackBar(context, e.message!);
+    }
+  }
+
+  //SignIn using Email
+  Future<void> signInWithEmail({
+    required String email,
+    required String password,
+    required BuildContext context,
+  }) async {
+    try {
+      await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      Navigator.of(context).pushAndRemoveUntil(
+        MaterialPageRoute(
+          builder: (context) {
+            return const HomeScreen();
+          },
+        ),
+        (route) => false,
+      );
+    } on FirebaseAuthException catch (e) {
+      displaySnackBar(context, e.message!);
+    }
+  }
+
+  // GOOGLE SIGN IN
+  Future<void> signInWithGoogle(BuildContext context) async {
+    try {
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+      final GoogleSignInAuthentication? googleAuth =
+          await googleUser?.authentication;
+
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      // ignore: unused_local_variable
+      UserCredential userCredential =
+          await _auth.signInWithCredential(credential);
+      Navigator.of(context).pushAndRemoveUntil(
+        MaterialPageRoute(
+          builder: (context) {
+            return const HomeScreen();
+          },
+        ),
+        (route) => false,
+      );
+    } on FirebaseAuthException catch (e) {
+      displaySnackBar(context, e.message!);
+    }
+  }
+
+  //PHONE SIGN IN
+  Future<void> phoneSignIn({
+    required BuildContext context,
+    required String phoneNumber,
+    required VoidCallback onSignInSuccess,
+  }) async {
+    try {
+      final TextEditingController codeController = TextEditingController();
+      await _auth.verifyPhoneNumber(
+        phoneNumber: phoneNumber,
+        verificationCompleted: (PhoneAuthCredential credential) async {
+          await _auth.signInWithCredential(credential);
+          onSignInSuccess();
+        },
+        verificationFailed: (e) {
+          displaySnackBar(context, e.message!);
+        },
+        codeSent: (verificationId, forceResendingToken) async {
+          showOTPDialog(
+            context: context,
+            codeController: codeController,
+            onPressed: () async {
+              PhoneAuthCredential credential = PhoneAuthProvider.credential(
+                verificationId: verificationId,
+                smsCode: codeController.text.trim(),
+              );
+
+              await _auth.signInWithCredential(credential);
+              Navigator.of(context).pop();
+            },
+          );
+        },
+        codeAutoRetrievalTimeout: (verificationId) {},
+      );
+    } on FirebaseAuthException catch (e) {
+      displaySnackBar(context, e.message!);
+    }
+  }
+
+  //Sign Out
+  Future<void> signOut(BuildContext context) async {
+    try {
+      await _auth.signOut();
+
+      Navigator.of(context).pushAndRemoveUntil(
+        MaterialPageRoute(
+          builder: (context) {
+            return const LoginScreen();
+          },
+        ),
+        (route) => false,
+      );
+    } on FirebaseAuthException catch (e) {
+      displaySnackBar(context, e.message!);
+    }
+  }
+
+  //CHECK FOR EXISTING PROFILE
+//   Future<bool> checkProfile() async {
+//     return FirebaseFirestore.instance
+//         .collection('users')
+//         .doc(user.uid)
+//         .get()
+//         .then((value) {
+//       if (value.exists) {
+//         return true;
+//       } else {
+//         return false;
+//       }
+//     });
+//   }
+}

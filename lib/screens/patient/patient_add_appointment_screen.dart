1cfffeca51e8ba6ecf09c73630ab5162fbb8b009
Shashank Sundar <shashanksundar16@gmail.com>
2024-04-11 13:22:38 +0530

    Doctor choice dropdown working in patient appointment booking page

--- lib/screens/patient/patient_add_appointment_screen.dart
+++ lib/screens/patient/patient_add_appointment_screen.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: avoid_print, use_build_context_synchronously
 
+import 'dart:developer';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:medimind_app/models/patients/patient_upcoming_appointment_model.dart';
@@ -7,6 +9,17 @@ import 'package:medimind_app/services/firebase_auth_methods.dart';
 import 'package:medimind_app/widgets/auth/auth_icon_button.dart';
 import 'package:medimind_app/widgets/patient/p_upcoming_appt_formfield.dart';
 import 'package:provider/provider.dart';
+import 'package:animated_custom_dropdown/custom_dropdown.dart';
+
+class DoctorData {
+  final String name;
+  final String hospital;
+
+  DoctorData({
+    required this.name,
+    required this.hospital,
+  });
+}
 
 class PatientAddAppointmentScreen extends StatefulWidget {
   final VoidCallback refreshData;
@@ -27,6 +40,26 @@ class _PatientAddAppointmentScreenState
   final TextEditingController hospitalController = TextEditingController();
   final TextEditingController dateController = TextEditingController();
   final TextEditingController timeController = TextEditingController();
+
+  late Future<List<DoctorData>> doctorData;
+
+  Future<List<DoctorData>> getDoctorsData() async {
+    final QuerySnapshot<Map<String, dynamic>> data =
+        await FirebaseFirestore.instance.collection("doctors").get();
+
+    return data.docs.map((item) {
+      return DoctorData(
+        name: item.data()["name"] as String,
+        hospital: item.data()["doctor_hospital"] as String,
+      );
+    }).toList();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    doctorData = getDoctorsData();
+  }
 
   late final PatientAppointment patientAppointment = PatientAppointment(
     doctorName: "",
@@ -61,62 +94,96 @@ class _PatientAddAppointmentScreenState
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-          child: Form(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                PatientUpcomingApptFormWidget(
-                  labelText: "Choose a doctor",
-                  controller: doctorController,
-                  fieldType: 0,
-                ),
-                PatientUpcomingApptFormWidget(
-                  labelText: "Choose a hospital",
-                  controller: hospitalController,
-                  fieldType: 0,
-                ),
-                PatientUpcomingApptFormWidget(
-                  labelText: "Choose a date",
-                  controller: dateController,
-                  fieldType: 1,
-                ),
-                PatientUpcomingApptFormWidget(
-                  labelText: "Choose a time",
-                  controller: timeController,
-                  fieldType: 2,
-                ),
-                AuthIconButton(
-                  labelText: "Book Appointment",
-                  isSvg: false,
-                  icon: Icons.add,
-                  onPress: () async {
-                    try {
-                      patientAppointment.doctorName = doctorController.text;
-                      patientAppointment.hospitalName = hospitalController.text;
-                      patientAppointment.date = dateController.text;
-                      patientAppointment.time = timeController.text;
+          child: FutureBuilder(
+            future: doctorData,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return Center(child: Text('Error: ${snapshot.error}'));
+              } else {
+                List<DoctorData> doctorDataResult = snapshot.data!;
+                return Form(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text("Choose a doctor"),
+                      const SizedBox(height: 20),
+                      CustomDropdown(
+                        decoration: const CustomDropdownDecoration(
+                          closedFillColor: Color.fromARGB(255, 255, 242, 242),
+                        ),
+                        items: doctorDataResult
+                            .map((doctor) =>
+                                '${doctor.name} - ${doctor.hospital}')
+                            .toList(),
+                        onChanged: (value) {
+                          // final selectedDoctor = value as DoctorData;
+                          // final doctorName = selectedDoctor.name;
+                          // final hospitalName = selectedDoctor.hospital;
+                          List<String> parts = value.split(' - ');
+                          String doctorName = parts[0];
+                          String hospitalName = parts[1];
 
-                      CollectionReference appointments = FirebaseFirestore
-                          .instance
-                          .collection("patient_upcoming_appointments")
-                          .doc(user.uid)
-                          .collection("appointment");
+                          print(doctorName);
+                          print(hospitalName);
 
-                      await appointments.add({
-                        'doctorName': patientAppointment.doctorName,
-                        'hospitalName': patientAppointment.hospitalName,
-                        'date': patientAppointment.date,
-                        'time': patientAppointment.time,
-                      });
-                      widget.refreshData();
-                      Navigator.of(context).pop();
-                    } catch (e) {
-                      print('Error uploading appointment: $e');
-                    }
-                  },
-                ),
-              ],
-            ),
+                          setState(() {
+                            doctorController.text = doctorName;
+                            hospitalController.text = hospitalName;
+                          });
+                        },
+                        initialItem:
+                            '${doctorDataResult[0].name} - ${doctorDataResult[0].hospital}',
+                      ),
+                      const SizedBox(height: 20),
+                      PatientUpcomingApptFormWidget(
+                        labelText: "Choose a date",
+                        controller: dateController,
+                        fieldType: 1,
+                      ),
+                      PatientUpcomingApptFormWidget(
+                        labelText: "Choose a time",
+                        controller: timeController,
+                        fieldType: 2,
+                      ),
+                      AuthIconButton(
+                        labelText: "Book Appointment",
+                        isSvg: false,
+                        icon: Icons.add,
+                        onPress: () async {
+                          try {
+                            patientAppointment.doctorName =
+                                doctorController.text;
+                            patientAppointment.hospitalName =
+                                hospitalController.text;
+                            patientAppointment.date = dateController.text;
+                            patientAppointment.time = timeController.text;
+
+                            CollectionReference appointments = FirebaseFirestore
+                                .instance
+                                .collection("patient_upcoming_appointments")
+                                .doc(user.uid)
+                                .collection("appointment");
+
+                            await appointments.add({
+                              'doctorName': patientAppointment.doctorName,
+                              'hospitalName': patientAppointment.hospitalName,
+                              'date': patientAppointment.date,
+                              'time': patientAppointment.time,
+                            });
+                            widget.refreshData();
+                            Navigator.of(context).pop();
+                          } catch (e) {
+                            print('Error uploading appointment: $e');
+                          }
+                        },
+                      ),
+                    ],
+                  ),
+                );
+              }
+            },
           ),
         ),
       ),
--- lib/widgets/patient/p_upcoming_appt_formfield.dart
+++ lib/widgets/patient/p_upcoming_appt_formfield.dart
@@ -38,10 +38,11 @@ class _PatientUpcomingApptFormWidgetState
               FocusScope.of(context).requestFocus(FocusNode());
 
               date = await showDatePicker(
-                  context: context,
-                  initialDate: DateTime.now(),
-                  firstDate: DateTime(1900),
-                  lastDate: DateTime(2100)) as DateTime;
+                context: context,
+                initialDate: DateTime.now(),
+                firstDate: DateTime(1900),
+                lastDate: DateTime(2100),
+              ) as DateTime;
 
               String formattedDate = DateFormat('yyyy-MM-dd').format(date);
               widget.controller.text = formattedDate;
